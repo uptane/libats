393c22da9e23325d22dc5eb322cc32b901f20ec8
Simão Mata <simao@users.noreply.github.com>
2022-06-15 18:03:18 +0200

    Use logger when db mx bean fails

--- libats-slick/src/main/scala/com/advancedtelematic/libats/slick/monitoring/DatabaseMetrics.scala
+++ libats-slick/src/main/scala/com/advancedtelematic/libats/slick/monitoring/DatabaseMetrics.scala
@@ -2,13 +2,15 @@ package com.advancedtelematic.libats.slick.monitoring
 
 import java.util
 import java.lang.management.ManagementFactory
-
 import javax.management.{JMX, ObjectName}
 import com.advancedtelematic.metrics.MetricsSupport
 import com.codahale.metrics.{Gauge, Metric, MetricSet}
+import com.zaxxer.hikari.HikariPoolMXBean
+import org.slf4j.LoggerFactory
 import slick.jdbc.JdbcDataSource
 import slick.jdbc.hikaricp.HikariCPJdbcDataSource
 import slick.util.AsyncExecutorMXBean
+
 import scala.collection.JavaConverters._
 import java.lang.management.ManagementFactory
 import java.util
@@ -17,6 +19,8 @@ import scala.util.control.NonFatal
 
 trait DatabaseMetrics { self: MetricsSupport =>
   val dbSource: JdbcDataSource
+
+  private lazy val log = LoggerFactory.getLogger(this.getClass)
 
   val hikariDatasource = {
     val hkds = dbSource.asInstanceOf[HikariCPJdbcDataSource]
@@ -29,7 +33,7 @@ trait DatabaseMetrics { self: MetricsSupport =>
       f.apply()
     } catch {
       case NonFatal(ex) =>
-        println(s"slick jmx mbean not ready yet, waiting for database to connect? ${ex.getMessage}")
+        log.warn(s"slick jmx mbean not ready yet, waiting for first db connection to trigger mbean register? ${ex.getClass}/${ex.getMessage}")
         zero
     }
   }
@@ -37,14 +41,14 @@ trait DatabaseMetrics { self: MetricsSupport =>
   val executorMXBean = {
     val mbeanServer = ManagementFactory.getPlatformMBeanServer
     val poolName = hikariDatasource.hconf.getPoolName
-    val mbean = JMX.newMXBeanProxy(mbeanServer, new ObjectName( s"slick:type=AsyncExecutor,name=$poolName"), classOf[AsyncExecutorMXBean])
+    val slickBean = JMX.newMXBeanProxy(mbeanServer, new ObjectName( s"slick:type=AsyncExecutor,name=$poolName"), classOf[AsyncExecutorMXBean])
     metricRegistry.register(s"slick.$poolName", new MetricSet {
       override def getMetrics: util.Map[String, Metric] = {
         Map[String, Metric](
-          "threads.active" -> gauge(() => mbean.getActiveThreads, 0),
-          "threads.max" -> gauge(() => mbean.getMaxThreads, 0),
-          "queue.size" -> gauge(() => mbean.getQueueSize, 0),
-          "queue.maxsize" -> gauge(() => mbean.getMaxQueueSize, 0)
+          "threads.active" -> gauge(() => slickBean.getActiveThreads, 0),
+          "threads.max" -> gauge(() => slickBean.getMaxThreads, 0),
+          "queue.size" -> gauge(() => slickBean.getQueueSize, 0),
+          "queue.maxsize" -> gauge(() => slickBean.getMaxQueueSize, 0)
         ).asJava
       }
     })
